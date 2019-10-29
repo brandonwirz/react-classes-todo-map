4fac5f74adcfc32814a25b6eb74225aa84027916
Brandon Wirz <bwirz75@gmail.com>
2019-10-29 14:45:17 -0600

    updating functional component to class based

--- src-todos/App.js
+++ src-todos/App.js
@@ -5,21 +5,47 @@ I've created a js file with some todos data in it, which I'm imported into this 
 
 Challenge: Using the array map method, render a child component for each todo item in the todosData array and pass the relevant data to it.
 */
+//functional
+// import React from "react"
+// import TodoItem from "./TodoItem"
+// import todosData from "./todosData"
+//
+// function App() {
+//     const todoItems = todosData.map(item => <TodoItem key={item.id} item={item}/>)
+//
+//     return (
+//         <div className="todo-list">
+//             {todoItems}
+//         </div>
+//     )
+// }
+//
+// export default App
 
+//class component
 import React from "react"
 import TodoItem from "./TodoItem"
 import todosData from "./todosData"
 
-span  
-function App() {
-    const todoItems = todosData.map(item =>  <TodoItem key={item.id} item={item}/>)
 
-    return (
-        <div className="todo-list">
-            {todoItems}
-        </div>
+class App extends React.Component {
+  constructor() {
+    super()
+    this.state = {
+      todos: todosData
+    }
+  }
+
+render() {
+    const todoItems = this.state.todos.map(item =>  <TodoItem key={item.id} item={item}/>)
+
+  return (
+      <div className="todo-list">
+          {todoItems}
+      </div>
     )
+  }
 }
-div
+
 
 export default App
